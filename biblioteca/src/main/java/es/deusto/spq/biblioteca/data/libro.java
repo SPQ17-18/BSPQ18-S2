6452f2a1b6b5a3106aaeede64b0a845a3ba90866
Odlok <koldopellicer@opendeusto.es>
2018-04-10 11:23:35 +0200

    Logica libro done #B18S2-34 add work 1h

---
+++ biblioteca/src/main/java/es/deusto/spq/biblioteca/data/libro.java
@@ -0,0 +1,54 @@
+package es.deusto.spq.biblioteca.data;
+
+public class libro {
+	
+	int isbn;
+	String titulo;
+	String autor;
+	String editorial;
+	
+	public libro(int isbn, String titulo, String autor, String editorial) {
+		this.isbn = isbn;
+		this.titulo = titulo;
+		this.autor = autor;
+		this.editorial = editorial;
+	}
+
+	public int getIsbn() {
+		return isbn;
+	}
+
+	public void setIsbn(int isbn) {
+		this.isbn = isbn;
+	}
+
+	public String getTitulo() {
+		return titulo;
+	}
+
+	public void setTitulo(String titulo) {
+		this.titulo = titulo;
+	}
+
+	public String getAutor() {
+		return autor;
+	}
+
+	public void setAutor(String autor) {
+		this.autor = autor;
+	}
+
+	public String getEditorial() {
+		return editorial;
+	}
+
+	public void setEditorial(String editorial) {
+		this.editorial = editorial;
+	}
+	
+	@Override
+	public String toString() {
+		return "libro [isbn=" + isbn + ", titulo=" + titulo + ", autor=" + autor + ", editorial=" + editorial + "]";
+	}
+
+}
