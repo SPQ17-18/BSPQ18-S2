eda31b50cd29f24f9da3c0323660938e3ed05ed4
Odlok <koldopellicer@opendeusto.es>
2018-04-11 09:41:05 +0200

    Minor change

--- biblioteca/src/main/java/es/deusto/spq/biblioteca/data/catalogo.java
+++ biblioteca/src/main/java/es/deusto/spq/biblioteca/data/catalogo.java
@@ -10,11 +10,11 @@ public class catalogo {
 	
 	public catalogo() {
 		//Algunos libros de muestra
-		libro libro1 = new libro(1, "LAS ALMAS DE BRANDON", "CESAR BRANDON", "S.L.U. ESPASA LIBROS");
-		libro libro2 = new libro(2, "MEMORIA DEL COMUNISMO", "FEDERICO JIMENEZ LOSANTOS", "LA ESFERA DE LOS LIBROS");
-		libro libro3 = new libro(3, "FESTIN DE CUERVOS, CANCION DE HIELO Y FUEGO IV", "GEORGE R.R. MARTIN", "GIGAMESH");
-		libro libro4 = new libro(4, "FYellowstar: Conviértete en un campeón de League of Legends", "BORA KIM ", "EDITORIAL PLANETA S.A");
-		libro libro5 = new libro(5, "PACO JEMEZ: GRANDES EXITOS EN EL RAYO VALLECANO", "JOSE ANTONIO CAMACHO", "FURIA VALLECANA");
+		libro libro1 = new libro(1, "LAS ALMAS DE BRANDON", "CESAR BRANDON", "S.L.U. ESPASA LIBROS", false);
+		libro libro2 = new libro(2, "MEMORIA DEL COMUNISMO", "FEDERICO JIMENEZ LOSANTOS", "LA ESFERA DE LOS LIBROS", false);
+		libro libro3 = new libro(3, "FESTIN DE CUERVOS, CANCION DE HIELO Y FUEGO IV", "GEORGE R.R. MARTIN", "GIGAMESH", false);
+		libro libro4 = new libro(4, "FYellowstar: Conviértete en un campeón de League of Legends", "BORA KIM ", "EDITORIAL PLANETA S.A", false);
+		libro libro5 = new libro(5, "PACO JEMEZ: GRANDES EXITOS EN EL RAYO VALLECANO", "JOSE ANTONIO CAMACHO", "FURIA VALLECANA", false);
 
 		listaLibros.add(libro1);
 		listaLibros.add(libro2);
