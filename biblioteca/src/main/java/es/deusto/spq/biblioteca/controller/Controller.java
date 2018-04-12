b5553933ab02e53bda68af8956ceadcf41ec9e75
arianefernandez <ariane.fernandez@opendeusto.es>
2018-04-12 22:15:38 +0200

    Cambios multiples

--- biblioteca/src/main/java/es/deusto/spq/biblioteca/controller/Controller.java
+++ biblioteca/src/main/java/es/deusto/spq/biblioteca/controller/Controller.java
@@ -1,8 +1,10 @@
 package es.deusto.spq.biblioteca.controller;
 
 import java.rmi.RemoteException;
+import java.util.ArrayList;
 
 import es.deusto.spq.biblioteca.client.Client;
+import es.deusto.spq.biblioteca.data.Reserva;
 
 public class Controller {
 	@SuppressWarnings("unused")
@@ -24,11 +26,13 @@ public class Controller {
 
 	public static void main(String[] args) throws RemoteException {
 		Controller c = new Controller(args);
+		c.getCl().getService().anyadirSala("S1",10);
+		c.getCl().getService().anyadirSala("S2",8);
 		c.getCl().getService().anyadirReserva("S1", "12345678X", "11/04/18", "21:20", 3);
 		c.getCl().getService().anyadirReserva("S2", "23456789Y", "12/05/18", "19:26", 2);
 		Boolean disponible=c.getCl().getService().consultarDisponibilidad("S1", "11/04/18", "10:00");
 		 if(disponible) {
-		 c.getCl().getService().anyadirReserva("S3", "34567890X", "11/04/18", "21:20", 4);
+		 c.getCl().getService().anyadirReserva("S1", "34567890X", "11/04/18", "21:20", 4);
 		 }
 	}
 }
--- biblioteca/src/main/java/es/deusto/spq/biblioteca/remote/IBiblioteca.java
+++ biblioteca/src/main/java/es/deusto/spq/biblioteca/remote/IBiblioteca.java
@@ -2,14 +2,18 @@ package es.deusto.spq.biblioteca.remote;
 
 import java.rmi.Remote;
 import java.rmi.RemoteException;
+import java.util.ArrayList;
+import java.util.List;
 
 import es.deusto.spq.biblioteca.data.Libro;
+import es.deusto.spq.biblioteca.data.Reserva;
 
 public interface IBiblioteca extends Remote{
 	
 	public Libro buscarLibro(String nombre) throws RemoteException;
 	public void almacenarLibro(int isbn, String nombre, String autor, String editorial, boolean isReservado) throws RemoteException;
 	public void anyadirReserva(String id_Sala, String dni_respon, String fecha, String hora, int plazas )throws RemoteException;
+	public void anyadirSala(String id_sala, int capacidad)throws RemoteException;
 	public boolean consultarDisponibilidad(String Id_Sala, String fecha, String hora)throws RemoteException;
 	//Añadir lo que falta
 }
