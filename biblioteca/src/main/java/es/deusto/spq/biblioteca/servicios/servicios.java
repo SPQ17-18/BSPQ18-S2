f9a471c3941f02e6eb02e507a1e88332286f9ade
julen <santinazcona@opendeusto.es>
2018-04-12 19:16:40 +0200

    Pequeños cambios.

--- biblioteca/src/main/java/es/deusto/spq/biblioteca/servicios/servicios.java
+++ biblioteca/src/main/java/es/deusto/spq/biblioteca/servicios/servicios.java
@@ -4,7 +4,6 @@ import java.util.ArrayList;
 import java.util.List;
 
 import es.deusto.spq.biblioteca.data.reserva;
-import es.deusto.spq.biblioteca.db.DB_reserva;
 
 public class servicios extends Thread {
 //	public int reservaSala(String id_sala, int plazasReserva){
