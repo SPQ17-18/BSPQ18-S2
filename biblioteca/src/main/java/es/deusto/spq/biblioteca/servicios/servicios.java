c5840d1a0dd289753b26f4e24995565b279937df
byJonMartinez <jonmartinezq@opendeusto.es>
2018-04-12 19:21:25 +0200

    Prueba Actualizar Reservas

--- biblioteca/src/main/java/es/deusto/spq/biblioteca/servicios/servicios.java
+++ biblioteca/src/main/java/es/deusto/spq/biblioteca/servicios/servicios.java
@@ -7,9 +7,13 @@ import es.deusto.spq.biblioteca.data.reserva;
 import es.deusto.spq.biblioteca.db.DB_reserva;
 
 public class servicios extends Thread {
-//	public int reservaSala(String id_sala, int plazasReserva){
-//		List<reserva> joker1 = new ArrayList<>();
-//		joker1 = DB_reserva.getInstance().getListaEntera();
-//	}
+	
+	public int reservaSala(String id_sala, int plazasReserva){
+		List<reserva> listaDeReservas = new ArrayList<>();
+		listaDeReservas = DB_reserva.getInstance().getListaEntera();
+		//getListaReservas.consultarDisponibilidad(String Id_Sala, String fecha, String hora)
+	}
 
+	
+	
 }
