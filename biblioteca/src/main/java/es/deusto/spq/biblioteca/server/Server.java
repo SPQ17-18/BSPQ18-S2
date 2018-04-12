bfeb8bf53a1656d46f510a0afe76b597b950bf5f
arianefernandez <ariane.fernandez@opendeusto.es>
2018-04-12 17:49:03 +0200

    El Servidor FUNCIONA :))))

--- biblioteca/src/main/java/es/deusto/spq/biblioteca/server/Server.java
+++ biblioteca/src/main/java/es/deusto/spq/biblioteca/server/Server.java
@@ -18,12 +18,10 @@ public class Server {
 			IBiblioteca server = new Biblioteca(args[0],Integer.parseInt(args[1]));
 			Naming.rebind(name, server);
 			System.out.println("* Servidor '" + name + "' activo y esperando...");
-			
-			//Para que este todo el rato escuchando el servidor
+//Para que este todo el rato escuchando el servidor
 			java.io.InputStreamReader inputStreamReader = new java.io.InputStreamReader ( System.in );
 			java.io.BufferedReader stdin = new java.io.BufferedReader ( inputStreamReader );
-			String line  = stdin.readLine();
-		} catch (Exception e) {
+			String line  = stdin.readLine();		} catch (Exception e) {
 			System.err.println("- Error en el servidor CRAI: " + e.getMessage());
 			e.printStackTrace();
 		}
