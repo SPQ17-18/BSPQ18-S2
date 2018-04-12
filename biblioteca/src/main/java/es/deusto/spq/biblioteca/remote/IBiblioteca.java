dd7ed726b0fe1de66c36a35fa7a39f599c9acd4c
Odlok <koldopellicer@opendeusto.es>
2018-04-12 20:47:38 +0200

    Ya me funciona el servidor lol

--- biblioteca/src/main/java/es/deusto/spq/biblioteca/remote/IBiblioteca.java
+++ biblioteca/src/main/java/es/deusto/spq/biblioteca/remote/IBiblioteca.java
@@ -10,6 +10,6 @@ public interface IBiblioteca extends Remote{
 	public Libro buscarLibro(String nombre) throws RemoteException;
 	public void almacenarLibro(int isbn, String nombre, String autor, String editorial, boolean isReservado) throws RemoteException;
 	public void anyadirReserva(String id_Sala, String dni_respon, String fecha, String hora, int plazas )throws RemoteException;
-	public boolean consultarDisponibilidad(String Id_Sala, String fecha, String hora);
+	public boolean consultarDisponibilidad(String Id_Sala, String fecha, String hora)throws RemoteException;
 	//Añadir lo que falta
 }
