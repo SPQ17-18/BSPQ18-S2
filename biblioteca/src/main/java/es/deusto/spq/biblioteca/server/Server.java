8907844311d9e7d32b369af17452b52860e8a7bd
Odlok <koldopellicer@opendeusto.es>
2018-04-12 19:16:04 +0200

    Minor changes. El servidor ya escucha todo el rato.

--- biblioteca/src/main/java/es/deusto/spq/biblioteca/server/Server.java
+++ biblioteca/src/main/java/es/deusto/spq/biblioteca/server/Server.java
@@ -18,6 +18,11 @@ public class Server {
 			IBiblioteca server = new Biblioteca(args[3],Integer.parseInt(args[4]));
 			Naming.rebind(name, server);
 			System.out.println("* Servidor '" + name + "' activo y esperando...");
+			
+			//Para que este todo el rato escuchando el servidor
+			java.io.InputStreamReader inputStreamReader = new java.io.InputStreamReader ( System.in );
+			java.io.BufferedReader stdin = new java.io.BufferedReader ( inputStreamReader );
+			String line  = stdin.readLine();
 		} catch (Exception e) {
 			System.err.println("- Error en el servidor EasyBooking: " + e.getMessage());
 			e.printStackTrace();
