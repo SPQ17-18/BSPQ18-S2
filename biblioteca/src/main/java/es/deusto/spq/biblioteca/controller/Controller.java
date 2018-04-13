1b7580c0778e51fd99178740ca19f5040dac9c93
arianefernandez <ariane.fernandez@opendeusto.es>
2018-04-13 10:35:12 +0200

    Cambios

--- biblioteca/src/main/java/es/deusto/spq/biblioteca/controller/Controller.java
+++ biblioteca/src/main/java/es/deusto/spq/biblioteca/controller/Controller.java
@@ -6,7 +6,6 @@ import java.util.ArrayList;
 import es.deusto.spq.biblioteca.client.Client;
 import es.deusto.spq.biblioteca.data.Reserva;
 
-
 public class Controller {
 	@SuppressWarnings("unused")
 	private Client cl;
@@ -15,9 +14,9 @@ public class Controller {
 
 		cl = new Client();
 		cl.setService(args);
-		
+
 	}
-	
+
 	public Client getCl() {
 		return cl;
 	}
@@ -26,18 +25,22 @@ public class Controller {
 		this.cl = cl;
 	}
 
-public static void main(String[] args) throws RemoteException {
+	public static void main(String[] args) throws RemoteException {
 
 		new Controller(args);
-		
-		Controller c = new Controller(args);
-		//Almacenamos libros 
 
-		c.getCl().getService().almacenarLibro(1, "Las almas de Brandom", "Cesar Brandom", "S.L.U. Espasa Libros", false);
-		c.getCl().getService().almacenarLibro(2, "Festin de cuervos, Cancion de Hielo y fuego IV", "George R.R. Martin", "Gigamesh", false);
-		c.getCl().getService().almacenarLibro(3, "FYellowstar: Conviértete en un campeón de League of Legends", "Bora Kim ", "Editorial Planeta S.A", false);	
-		
-		//Busqueda de libro por nombre
+		Controller c = new Controller(args);
+		// Almacenamos libros
+
+		c.getCl().getService().almacenarLibro(1, "Las almas de Brandom", "Cesar Brandom", "S.L.U. Espasa Libros",
+				false);
+		c.getCl().getService().almacenarLibro(2, "Festin de cuervos, Cancion de Hielo y fuego IV", "George R.R. Martin",
+				"Gigamesh", false);
+		c.getCl().getService().almacenarLibro(3, "FYellowstar: Conviértete en un campeón de League of Legends",
+				"Bora Kim ", "Editorial Planeta S.A", false);
+
+		// Busqueda de libro por nombre
 		c.getCl().getService().buscarLibro("Festin de cuervos, Cancion de Hielo y fuego IV");
 		c.getCl().getService().buscarLibro("Paco Jemez: Grandes exitos en el Rayo Vallecano");
-	}	}}
+	}
+}
