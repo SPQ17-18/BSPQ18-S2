d6a25e6759614f19f69260eece0b7aa1215e7eee
arianefernandez <ariane.fernandez@opendeusto.es>
2018-04-29 20:10:57 +0200

    Cambios

--- biblioteca/src/test/java/es/deusto/spq/biblioteca/ReservaDAOTest.java
+++ biblioteca/src/test/java/es/deusto/spq/biblioteca/ReservaDAOTest.java
@@ -49,34 +49,5 @@ public class ReservaDAOTest {
 			assertEquals(r.getFecha(), rn.getFecha());
 			assertEquals(r.getHora(), rn.getHora());
 			assertEquals(r.getPlazas(), rn.getPlazas());
-	}
-	
-	@Test
-	public void testEditarReserva() throws RemoteException {
-		Reserva r = new Reserva("R5", "S2", "20304050A", "29-04-2018", "18:42", 8);
-		when( dao.devolverReserva("20304050A", "29-04-2018", "18:42")).thenReturn(r);
-		b.editarReserva(r, "19:00");
-		ArgumentCaptor<Reserva> reservaCaptor = ArgumentCaptor.forClass( Reserva.class );
-		verify (dao).editarReserva(reservaCaptor.capture(), "19:00");
-		Reserva rn = reservaCaptor.getValue();
-		System.out.println("Editando reserva");
-		assertEquals( "19:00", rn.getHora());
-	}
-	
-	@Test(expected=RemoteException.class)
-	public void testVerReservaNoExiste() throws RemoteException {	
-		when( dao.devolverReserva("19182138S", "29-04-2018", "18:27")).thenReturn( null );
-		System.out.println("No existe reserva");
-		b.verReservas("1918218S");
-			
-	}
-	
-	@Test
-	public void testVerReserva() throws RemoteException {
-		// Setting up the test data
-		Reserva r = new Reserva("R5", "S2", "20304050A", "29-04-2018", "18:42", 8);
-		when( dao.devolverReserva("20304050A", "29-04-2018", "18:42")).thenReturn(r);
-		b.verReservas("20304050A");	
-	}
-	
+	}	
 }
