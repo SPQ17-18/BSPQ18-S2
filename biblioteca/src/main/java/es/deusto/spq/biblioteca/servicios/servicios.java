155989d188ce867aba39a946755a264cde611b81
byJonMartinez <jonmartinezq@opendeusto.es>
2018-04-30 13:09:25 +0200

    Limpieza de código innecesario

--- biblioteca/src/main/java/es/deusto/spq/biblioteca/servicios/servicios.java
+++ biblioteca/src/main/java/es/deusto/spq/biblioteca/servicios/servicios.java
@@ -8,15 +8,6 @@ import es.deusto.spq.biblioteca.db.DB_reserva;
 
 public class servicios extends Thread {
 	
-	/**public int reservaSala(String id_sala, int plazasReserva){
-		List<reserva> listaDeReservas = new ArrayList<>();
-		listaDeReservas = DB_reserva.getInstance().getListaEntera();
-		getListaReservas.consultarDisponibilidad(String Id_Sala, String fecha, String hora)
-	}
-
-	public int prolongarEstancia() {
-		
-	}
-	*/
+	
 	
 }
