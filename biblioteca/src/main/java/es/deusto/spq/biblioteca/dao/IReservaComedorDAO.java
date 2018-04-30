9a6701e9ab8bc1f95db588520901b664a8f5c997
arianefernandez <ariane.fernandez@opendeusto.es>
2018-04-30 18:58:56 +0200

    #B18S2-90 add work 2m

---
+++ biblioteca/src/main/java/es/deusto/spq/biblioteca/dao/IReservaComedorDAO.java
@@ -0,0 +1,9 @@
+package es.deusto.spq.biblioteca.dao;
+
+import es.deusto.spq.biblioteca.data.ReservaMesa;
+
+public interface IReservaComedorDAO {
+	
+	public void anyadirReservaC(ReservaMesa r);
+	
+}
