f08099db1334b1b6ac2ac7249d5d5ec006a1f4fe
arianefernandez <ariane.fernandez@opendeusto.es>
2018-04-30 18:37:20 +0200

    #B18S2-80 add work 10m

---
+++ biblioteca/src/main/java/es/deusto/spq/biblioteca/data/ReservaMesa.java
@@ -0,0 +1,79 @@
+package es.deusto.spq.biblioteca.data;
+
+import javax.jdo.annotations.Inheritance;
+import javax.jdo.annotations.InheritanceStrategy;
+import javax.jdo.annotations.PersistenceCapable;
+
+@PersistenceCapable(detachable = "true")
+@Inheritance(strategy = InheritanceStrategy.NEW_TABLE)
+public class ReservaMesa {
+
+	String id_Comedor;
+	String id_Mesa;
+	String dni_respon;
+	String fecha;
+	String hora;
+	int Personas;
+	
+	public ReservaMesa() {
+		super();
+	}
+
+	public ReservaMesa(String id_Comedor, String id_Mesa, String dni_respon, String fecha, String hora, int personas) {
+		super();
+		this.id_Comedor = id_Comedor;
+		this.id_Mesa = id_Mesa;
+		this.dni_respon = dni_respon;
+		this.fecha = fecha;
+		this.hora = hora;
+		Personas = personas;
+	}
+
+	public String getId_Comedor() {
+		return id_Comedor;
+	}
+
+	public void setId_Comedor(String id_Comedor) {
+		this.id_Comedor = id_Comedor;
+	}
+
+	public String getId_Mesa() {
+		return id_Mesa;
+	}
+
+	public void setId_Mesa(String id_Mesa) {
+		this.id_Mesa = id_Mesa;
+	}
+
+	public String getDni_respon() {
+		return dni_respon;
+	}
+
+	public void setDni_respon(String dni_respon) {
+		this.dni_respon = dni_respon;
+	}
+
+	public String getFecha() {
+		return fecha;
+	}
+
+	public void setFecha(String fecha) {
+		this.fecha = fecha;
+	}
+
+	public String getHora() {
+		return hora;
+	}
+
+	public void setHora(String hora) {
+		this.hora = hora;
+	}
+
+	public int getPersonas() {
+		return Personas;
+	}
+
+	public void setPersonas(int personas) {
+		Personas = personas;
+	}	
+}
