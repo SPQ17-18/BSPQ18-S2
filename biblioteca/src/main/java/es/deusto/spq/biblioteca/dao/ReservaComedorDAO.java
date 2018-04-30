dfca4cbda14a63d9418b590303b4fb46265b7fe0
arianefernandez <ariane.fernandez@opendeusto.es>
2018-04-30 18:58:03 +0200

    #B18S2-90 add work 10m

---
+++ biblioteca/src/main/java/es/deusto/spq/biblioteca/dao/ReservaComedorDAO.java
@@ -0,0 +1,51 @@
+package es.deusto.spq.biblioteca.dao;
+
+import java.util.List;
+
+import javax.jdo.JDOHelper;
+import javax.jdo.PersistenceManager;
+import javax.jdo.PersistenceManagerFactory;
+import javax.jdo.Query;
+import javax.jdo.Transaction;
+
+import es.deusto.spq.biblioteca.data.Comedor;
+import es.deusto.spq.biblioteca.data.ReservaMesa;
+
+public class ReservaComedorDAO implements IReservaComedorDAO{
+
+	private PersistenceManagerFactory pmf;
+
+	public ReservaComedorDAO() {
+		pmf = JDOHelper.getPersistenceManagerFactory("datanucleus.properties");
+	}
+
+	@Override
+	public void anyadirReservaC(ReservaMesa r) {
+		// TODO Auto-generated method stub
+		PersistenceManager pm = pmf.getPersistenceManager();
+		Transaction tx = pm.currentTransaction();
+		try {
+			tx.begin();
+			Query<Comedor> query = pm.newQuery(Comedor.class);
+			@SuppressWarnings("unchecked")
+			List<Comedor> mesas = (List<Comedor>) query.execute();
+			for (Comedor m : mesas) {
+				if (m.getId_Comedor().equals(r.getId_Comedor())) {
+					m.getReservaMesas().add(r);
+					System.out.println("   * Guardando reserva de comedor: " + r.getId_Comedor());
+					pm.makePersistent(m);
+					tx.commit();
+				}
+			}
+		} catch (Exception ex) {
+			System.out.println("   $ Error guardando reserva: " + ex.getMessage());
+		} finally {
+			if (tx != null && tx.isActive()) {
+				tx.rollback();
+			}
+
+			pm.close();
+		}
+	}
+
+}
