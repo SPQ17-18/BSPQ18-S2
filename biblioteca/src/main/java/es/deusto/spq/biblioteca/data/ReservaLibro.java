22cb4099fd7dc737eb45af0cf4b60171d85b559f
Odlok <koldopellicer@opendeusto.es>
2018-05-19 18:01:06 +0200

    Arreglado reserva Libro #B18S2-143 add work 2h

--- biblioteca/src/main/java/es/deusto/spq/biblioteca/data/ReservaLibro.java
+++ biblioteca/src/main/java/es/deusto/spq/biblioteca/data/ReservaLibro.java
@@ -1,35 +1,30 @@
 package es.deusto.spq.biblioteca.data;
 
 import java.io.Serializable;
-import java.util.ArrayList;
-import java.util.List;
 
 import javax.jdo.annotations.Inheritance;
 import javax.jdo.annotations.InheritanceStrategy;
-import javax.jdo.annotations.Join;
 import javax.jdo.annotations.PersistenceCapable;
 
 @PersistenceCapable(detachable = "true")
 @Inheritance(strategy=InheritanceStrategy.NEW_TABLE)
 public class ReservaLibro implements Serializable {
 	
-	String id_reserva;
-	int isbn_libro;
-	String fecha;
-	String hora;
-	int plazas;
+	String isbn;
 
 	public ReservaLibro() {
 		
 	}
 
-	public ReservaLibro(String id_reserva, int isbn_libro, String dni_respon, String fecha, String hora, int plazas) {
+	public ReservaLibro(String isbn) {
 		super();
-		this.id_reserva = id_reserva;
-		this.isbn_libro = isbn_libro;
-		this.fecha = fecha;
-		this.hora = hora;
+		this.isbn = isbn;
 
+	}
+
+
+	public String getIsbn() {
+		return isbn;
 	}
 
 }
