1b58bbc15546470b8a07e7d2c74f8546d7ec0566
Odlok <koldopellicer@opendeusto.es>
2018-05-19 17:53:31 +0200

    Arreglado lo de disponiblilidad de los libros #B18S2-142 add work 2h

---
+++ biblioteca/src/main/java/es/deusto/spq/biblioteca/dao/ReservaLibroDAO.java
@@ -0,0 +1,101 @@
+package es.deusto.spq.biblioteca.dao;
+
+import java.util.List;
+
+import javax.jdo.JDOHelper;
+import javax.jdo.PersistenceManager;
+import javax.jdo.PersistenceManagerFactory;
+import javax.jdo.Query;
+import javax.jdo.Transaction;
+import org.apache.log4j.Logger;
+
+import es.deusto.spq.biblioteca.data.Libro;
+import es.deusto.spq.biblioteca.data.Reserva;
+import es.deusto.spq.biblioteca.data.ReservaLibro;
+
+public class ReservaLibroDAO implements IReservaLibroDAO {
+	
+	private PersistenceManagerFactory pmf;
+	private static final Logger logger = Logger.getLogger(ReservaLibroDAO.class);
+	
+	public ReservaLibroDAO() {
+		pmf = JDOHelper.getPersistenceManagerFactory("datanucleus.properties");
+	}
+
+	@Override
+	public void reservarLibro(ReservaLibro rl) {
+		// TODO Auto-generated method stub
+		PersistenceManager pm = pmf.getPersistenceManager();
+		Transaction tx = pm.currentTransaction();
+		try {
+			tx.begin();
+			Query<Libro> q = pm.newQuery(Libro.class);
+			
+			@SuppressWarnings("unchecked")
+			List<Libro> libros = (List<Libro>) q.execute();
+			
+			for(Libro l : libros) 
+			{
+				if(l.getIsbn().equals(rl.getIsbn())) {
+					l.getReservasLibros().add(rl);
+					
+					logger.info("   * Guardando reserva del libro: " + rl.getIsbn());
+
+					pm.makePersistent(l);
+					tx.commit();
+				}
+			}
+			
+		} catch (Exception ex) {
+			//System.out.println("   $ Error guardando reserva: " + ex.getMessage());
+			logger.error("   $ Error guardando reserva del libro:" + ex.getMessage());
+
+		} finally {
+			if (tx != null && tx.isActive()) {
+				tx.rollback();
+			}
+
+			pm.close();
+		}	
+		
+	}
+
+	@Override
+	public boolean consultarDisponibilidadLibro(String isbn) {
+		// TODO Auto-generated method stub
+		boolean isReservado = true;
+		
+		PersistenceManager pm = pmf.getPersistenceManager();
+		Transaction tx = pm.currentTransaction();
+		
+		try {
+			//System.out.println("   * Consultado disponibilidad de: " + Id_Sala);
+			logger.info("   * Consultado disponibilidad de: " + isbn);
+
+			tx.begin();
+			Query<ReservaLibro> query = pm.newQuery(ReservaLibro.class);
+			@SuppressWarnings("unchecked")
+			List<ReservaLibro> reservasLibros = (List<ReservaLibro>) query.execute();
+			for (ReservaLibro rl : reservasLibros) {
+				if (rl.getIsbn().equals(isbn)) {
+					// No hay salas disponibles
+					isReservado = false;
+				}
+			}
+			tx.commit();
+		} catch (Exception ex) {
+			//System.out.println("   $ Error consultando disponibilidad del libro: " + ex.getMessage());
+			logger.error("   $ Error consultando disponibilidad del libro " + ex.getMessage());
+
+		} finally {
+			if (tx != null && tx.isActive()) {
+				tx.rollback();
+			}
+
+			pm.close();
+		}
+		
+		return isReservado;
+	}
+
+}
