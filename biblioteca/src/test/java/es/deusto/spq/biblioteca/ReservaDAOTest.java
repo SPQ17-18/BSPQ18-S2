c3bab8b18bc01733bfb68f7a88e004a0a6c268ac
Mikel <mikel.colmenares@opendeusto.es>
2018-05-20 18:22:35 +0200

    SalaDAOTest y ReservaDAOTest

--- biblioteca/src/test/java/es/deusto/spq/biblioteca/ReservaDAOTest.java
+++ biblioteca/src/test/java/es/deusto/spq/biblioteca/ReservaDAOTest.java
@@ -5,6 +5,7 @@ import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
 import java.rmi.RemoteException;
+import java.util.ArrayList;
 
 import org.junit.Before;
 import org.junit.Test;
@@ -31,7 +32,6 @@ public class ReservaDAOTest {
 
 	@Before
 	public void setUp() throws Exception {
-		//Inicializado para todos los teses
 		b = new Biblioteca(dao);
 
 	}
@@ -50,12 +50,48 @@ public class ReservaDAOTest {
 			assertEquals(r.getHora(), rn.getHora());
 			assertEquals(r.getPlazas(), rn.getPlazas());
 	}
+	
+	@Test
+	public void consultarDisponibilidadTest() throws RemoteException {
+		Reserva r = new Reserva("R5", "S2", "20304050A", "29-04-2018", "18:42", 8);
+		dao.anyadirReserva(r);
+		assertEquals(false,dao.consultarDisponibilidad(r.getId_sala(), r.getFecha(), r.getHora()));
+	}
+	
+	@Test
+	public void devolverReservaTest() throws RemoteException {
+		Reserva r = new Reserva("R5", "S2", "20304050A", "29-04-2018", "18:42", 8);
+		dao.anyadirReserva(r);
+		Reserva n = dao.devolverReserva("20304050A", "29-04-2018", "18:42");
+		
+		assertEquals(r.getDni_respon(),n.getDni_respon());
+		assertEquals(r.getFecha(),n.getFecha());
+		assertEquals(r.getHora(),n.getHora());
+	}
+
+	@Test
+	public void verReservasTest() throws RemoteException {
+		Reserva r = new Reserva("R5", "S2", "20304050A", "29-04-2018", "18:42", 8);
+		dao.anyadirReserva(r);
+		ArgumentCaptor<Reserva> reservaCaptor = ArgumentCaptor.forClass( Reserva.class );
+		verify(dao).anyadirReserva(reservaCaptor.capture());
+		ArrayList<String> a = new ArrayList<String>();
+		System.out.println(r.getDni_respon());
+		a = dao.verReservas(r.getDni_respon());
+		String b="R5#20304050A#S2#29-04-2018#18:42#8";
+		String h = null;
+
+		for (String string : a) {
+			System.out.println(string);
+			h+=string;
+		}
+				
+		assertEquals("20304050A",h);
+		
+	}
+	
+	
+	
 }
-
-
-
-
-
-
 
 
--- biblioteca/src/test/java/es/deusto/spq/biblioteca/SalaDAOTest.java
+++ biblioteca/src/test/java/es/deusto/spq/biblioteca/SalaDAOTest.java
@@ -1,4 +1,4 @@
-/*package es.deusto.spq.biblioteca;
+package es.deusto.spq.biblioteca;
 
 import static org.junit.Assert.*;
 import static org.mockito.Mockito.verify;
@@ -13,6 +13,8 @@ import org.mockito.ArgumentCaptor;
 import org.mockito.Mock;
 import org.mockito.junit.MockitoJUnitRunner;
 import es.deusto.spq.biblioteca.dao.ISalaDAO;
+import es.deusto.spq.biblioteca.dao.SalaDAO;
+import es.deusto.spq.biblioteca.data.Reserva;
 import es.deusto.spq.biblioteca.data.Sala;
 import es.deusto.spq.biblioteca.remote.Biblioteca;
 import junit.framework.JUnit4TestAdapter;
@@ -20,5 +22,40 @@ import junit.framework.JUnit4TestAdapter;
 @RunWith(MockitoJUnitRunner.class)
 public class SalaDAOTest {
 
+	Biblioteca b;
+
+	@Mock
+	ISalaDAO dao;
+
+	public static junit.framework.Test suite() {
+		return new JUnit4TestAdapter(ReservaDAOTest.class);
+	}
+
+	@Before
+	public void setUp() throws Exception {
+		b = new Biblioteca(dao);
+
+	}
+
+	@Test
+	public void anyadirSala() throws RemoteException {
+		Sala s = new Sala("ID3",4);
+		b.anyadirSala(s.getId_sala(),s.getCapacidad());
+		ArgumentCaptor<Sala> reservaCaptor = ArgumentCaptor.forClass( Sala.class );
+		verify(dao).anyadirSala(reservaCaptor.capture());
+		System.out.println("Anyadiendo reserva ");
+		Sala rn=reservaCaptor.getValue();
+		
+		assertEquals(s.getId_sala(), rn.getId_sala());
+		assertEquals(s.getCapacidad(), rn.getCapacidad());
+		
+	}
+	@Test
+	public void consultarPlazas() throws RemoteException {
+		//Sala s = new Sala("ID3",2);		
+		//b.anyadirSala(s.getId_sala(),s.getCapacidad());
+		boolean bol =	dao.consultarPlazas("S1", 200);
+		assertEquals(false,bol );
+	}
+	
 }
-*/
