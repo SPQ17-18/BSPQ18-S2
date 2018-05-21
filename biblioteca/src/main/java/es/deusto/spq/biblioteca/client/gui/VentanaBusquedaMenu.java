db1536d2c5b6b117bc86932a40c9544cb88a5f10
byJonMartinez <jonmartinezq@opendeusto.es>
2018-05-21 19:37:51 +0200

    #B18S2-50 add work 1h

--- biblioteca/src/main/java/es/deusto/spq/biblioteca/client/gui/VentanaBusquedaMenu.java
+++ biblioteca/src/main/java/es/deusto/spq/biblioteca/client/gui/VentanaBusquedaMenu.java
@@ -45,6 +45,7 @@ public class VentanaBusquedaMenu extends JFrame {
 		contentPane.setLayout(null);
 		Image img = new ImageIcon(this.getClass().getResource("/menu3.jpg")).getImage();
 		
+		
 		JButton btnComprar = new JButton("Comprar");
 		btnComprar.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
